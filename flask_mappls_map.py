38bd13b12bd8da4c70b0610789e96b6ce05367a7
Darshan <darshanchoudhary2007@gmail.com>
2024-11-04 15:23:32 +0530

    added db and getSavedLoc

--- flask_mappls_map.py
+++ flask_mappls_map.py
@@ -1,11 +1,18 @@
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests
 import json
 import speech_recognition as sr
 import threading
+from flask_cors import CORS
+import pyrebase
+from config import config
 
 app = Flask(__name__)
+CORS(app)
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
 
 command = None
 
@@ -36,6 +43,12 @@ def map_view():
     return render_template(template_name_or_list="mappls_map.html",command=command)
 
     # 16.62662018, 49.2125578
-    
+@app.route("/getSavedLoc", methods = ["GET"])
+def getSavedLoc():
+    count = 1
+    data = db.child("savedLoc/").get().val()
+    data.pop(0)
+    return jsonify(data)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=3000)
